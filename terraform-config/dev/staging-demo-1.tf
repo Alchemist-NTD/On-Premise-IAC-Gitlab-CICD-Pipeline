c6e68d5f8a1a70e3ff454fec5483961a8e9977a9
Duy Nguyễn <76658438+Warlock-NTD@users.noreply.github.com>
2024-02-16 09:54:44 +0700

    Update staging-demo-1.tf

--- terraform-config/dev/staging-demo-1.tf
+++ terraform-config/dev/staging-demo-1.tf
@@ -24,7 +24,7 @@ resource "proxmox_vm_qemu" "stg_duy_1" {
 
   nameserver = "dns.enigma.com.vn"
   sshkeys = <<EOF
-  ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC+OaDIAozboLA3bjGYoe93ELdS9wTSEp4RpXtidz9vlkoPrjyqfWKO9QbO6NgQuZqGHtVyXPmMR0k5rI1VWXEjoAY7mOavizbyGWmKQhFxYu/FAS+ko2q9YdMGOgDUV5teMHQkgLMK9U1fzbT52S2REflh3dNyiAG5ihEAXwuScBI5z/LvQtrs0DzkXnKyKm0+4PHWE3BdsS313F5b3cqvrkIWVUoAL2QT9GRgbDy1RfUHhgy/OtfxIJr4ckEJZ2xfvJY1YtSZ2YrjbjxKixXMqtsFGodQN3Uz4i8WMB0O0z9ewpvzHHXqphjJfq9K2cFO7jtEkR7XU6nZBsEcx3epW5+vRa9rqYGE+STQI9nqF8NmjdhXn9ORHXAboQLjPfEQ5SCF8ZETSczfDfmLstQxRps+VAv2eUZg6D6j6Ft2syVtOUQQ/4tLJyzvRcsjtK/l14okH2ARByVzueFZVwSWd+TkSbIi9gP1/jpFA5rEM/xtEl3miKfNXeKLP+WJ+es= duy@duy
+    var.ssh_key
   EOF
 }
 
